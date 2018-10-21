c62185be7fa773234d2576c34e35873893fc1036
romane134 <43406444+romane134@users.noreply.github.com>
2018-10-21 11:15:55 +0200

    m

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -26,52 +26,37 @@ static int		ft_checkstop(static char buf)
 	return (0);
 }
 
-char			*ft_stock_buf(char **str, static char *buf)
+int 					ft_line(char **str, static char *buf const int fd)
 {
-	int a;
-	int b;
-	char *stock;
-
-	b = 0;
-	str[a] = ft_read();
-
-	if (a = ft_strchr(buf, '\n')) //on est au premier '\n'?
-		while (a < BUFF_SIZE)
-			str[a++] = stock[b++];
-	if (buf[BUFF_SIZE] == '\0')
-			ft_strcpy(str, stock);
-	return (stock);
+	char *tmp;
+	*str = ft_read(buf, fd, *str);
+	if (ft_strchr(buf, '\n')) //ca veut dire qu'on est sur un \n
+	{
+		if (ft_s)
+		//regarder dans le buf ce qu'il reste
+		return (1);
+	}
 }
+
 static char		*ft_read(static  char *buf, const int fd, char **str)
 {
 	int ret;
 	static char		buf[BUFF_SIZE + 1];
 	char *tmp;
-	while (BUFF_SIZE > 0)
-	{
-		if (str = ft_stock_buf)
-	}
+
+	str = NULL;
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0) // ecrase le text de la lecture precedente
 	{
-		buf[ret] = '\0';
-		if (!(ft_strchr(buf, '\n')))
+		if (ft_strchr(buf, '\n'))
+			return (buf);
+		else if (buf[ret] != '\0')
 		{
-			tmp = *str;
-			*str = ft_strjoin(*str, buf); //faire une fonction pour moove ces 3 lignes de merde (☞ﾟヮﾟ)☞
-			free(tmp);
-			return (&str);
+				buf[ret] = '\0';
+				*str = ft_strjoin(*str, buf); //voir si pointeur est NULL
+				return (*str);
 		}
-		if (tmp = ft_strchr(buf, '\n'))
-		{
-			*tmp = 0;
-			tmp = *str;
-			*str = ft_strjoin(*str, buf); //ici, mettre une fonction qui sotck le reste du buffer
-			free(tmp);
-		//	tmp = ft_strchr(buf, '\0');
-		//	*tmp = '\n';
-		*str = ft_stock(*str, buf /*....*/)
-			return (&str)
-		}
+		else
+			return (buf);
 	}
 	if (ret == 0)
 		return (0);
@@ -79,20 +64,47 @@ static char		*ft_read(static  char *buf, const int fd, char **str)
 
 int		get_next_line(const int fd, char **line)
 {
-	int			ret;
 	char		**str;
 	static char	*buf;
 
 	if (fd < 0 || !line || BUFF_SIZE <= 0)
 		return (-1);
-	*line = str;
-	if ('\n' |||| *str = ft_read(buf, fd, &str)) //j'ai lu jusq '\n'      &str difg
+if (ft_line(*str, buf) != 0)
+{
+	*line = &str;
+	return (1);
+}
+		return (1);
+	}
+	else if (ft_line(str, buf, fd) == 0)
 	{
- 		//finir
+
 	}
 
-	if (ft_read(buf ,fd, &str) == 0) //pas appeller ft_stock_buff plutot??
+	if (ft_read(buf ,fd, str) == 0) //pas appeller ft_stock_buff plutot??
 		return (0);
 
 	return (0);
 }
+
+
+/*
+if (!(ft_strchr(buf, '\n')))
+{
+	tmp = *str;
+	*str = ft_strjoin(*str, buf); //faire une fonction pour moove ces 3 lignes de merde (☞ﾟヮﾟ)☞
+	free(tmp);
+	return (&str);
+}
+if (tmp = ft_strchr(buf, '\n'))
+{
+	*tmp = 0;
+	tmp = *str;
+	*str = ft_strjoin(*str, buf); //ici, mettre une fonction qui sotck le reste du buffer
+	free(tmp);
+}
+//	tmp = ft_strchr(buf, '\0');
+//	*tmp = '\n';
+
+*str = ft_stock(*str, buf /*....*/)
+//return (&str)
