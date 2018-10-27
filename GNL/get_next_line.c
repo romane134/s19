ecb61636da09c97e5044cec1174eb8a9526ff018
Romane Lucas de couville <rlucas-d@e-r4-p6.s19.be>
2018-10-27 17:42:32 +0200

    ssfa

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,19 +6,26 @@
 /*   By: rlucas-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/12 17:31:26 by rlucas-d          #+#    #+#             */
-/*   Updated: 2018/10/22 12:37:59 by rlucas-d         ###   ########.fr       */
+/*   Updated: 2018/10/27 17:42:23 by rlucas-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-
-int		ft_check_buf(char *buf, int ret)
+char	*ft_check_rest(char *rest)
 {
-	if (buf[ret] == '\n')
-		return (1);
-	return (0);
+	int		i;
+
+	i = 0;
+	if (ft_strchr(rest, '\n'))
+	{
+		while (rest[i] != '\n')
+			i++;
+		rest[i] = '\0';
+		return (rest);
+	}
+	return (rest);
 }
 
 char	*ft_read_line(char *buf, const int fd)
@@ -26,55 +33,80 @@ char	*ft_read_line(char *buf, const int fd)
 	int				ret;
 	char			*str;
 	char			*tmp;
-	static char		*rest; //la premiere fois le rest == NULL non? donc que faire?
+	static char		*rest;
+	int				i;
 
-	rest = NULL;
-	str = ft_strnew(0); //je dois free str non??
+	if (rest == NULL)
+		rest = ft_strnew(0);
+	if (*rest)
+	{
+		rest = ft_check_rest(rest);
+		if (ft_strchr(rest, '\n'))
+		{
+			return (rest - 1);
+		}
+		str = rest;
+	}
+	else
+		str = ft_strnew(0);
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		if (rest == NULL)
-			rest = ft_strnew(0);
-		buf = ft_strjoin(rest, buf);
 		buf[ret] = '\0';
-
-		if (!(ft_strchr(buf, '\n'))) //ici on a pas de \n donc on relit encore la prochaine fois vu que pas de return
-		{
-			tmp = str;
-			str = ft_strjoin(str, buf);
-			free(tmp);
-		}
-		else
-		{
-			if (ft_check_buf(buf, ret) != 0) //ici on a un \n et il est a la fin du buf donc on peut return
-			{
-				tmp = str;
-				str = ft_strjoin(str, buf);
-				free(tmp);
-				return (str); //on return la ligne
-			}
-			else // on a un \n mais il y a autre chose apres
-			{
-				while (*str != '\n')
-					str++;
-				rest = ft_strncpy(rest, str, (ret - *str)); // on copie a partir du \n (la ou pointe str?) jusqu'a la fin de ret
-				return (str);
-			}
-		}
+		tmp = str;
+		str = ft_strjoin(str, buf);
+		free(tmp);
+		if ((ft_strchr(buf, '\n')))
+			break ;
+	}
+	if (str[ret - 1] == '\n')
+	{
+		str[ft_strlen(str) - 1] = '\0';
+		return (str);
+	}
+	else
+	{
+		i = 0;
+		while (str[i] != '\n' && str[i])
+			i++;
+		str[i] = '\0';
+		rest = ft_strdup(str + i + 1);
+		return (str);
 	}
 	if (ret == -1)
 		return (NULL);
 	return (str);
 }
 
-int			get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-	char		buf[BUFF_SIZE + 1];
+	char	buf[BUFF_SIZE + 1];
 
 	if (fd < 0 || !line || BUFF_SIZE <= 0)
 		return (-1);
 	if (!(*line = ft_read_line(buf, fd)))
+	{
+		free(ft_read_line(buf, fd));
 		return (-1);
+	}
 	if (**line == 0)
 		return (0);
 	return (1);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int		main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("file.txt", O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
+	{
+		printf("GNL--%s\n", line);
+		free(line);
+		printf("!!!!!!!!!!!!!!!!\n");
+	}
+	close(fd);
 }
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,17 +6,18 @@
 /*   By: rlucas-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/12 17:31:31 by rlucas-d          #+#    #+#             */
-/*   Updated: 2018/10/22 12:32:52 by rlucas-d         ###   ########.fr       */
+/*   Updated: 2018/10/27 17:14:16 by rlucas-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LIGNE_H
 # define GET_NEXT_LIGNE_H
-# define BUFF_SIZE 13
+# define BUFF_SIZE 5
 
 #include "libft/libft.h"
 #include <sys/types.h>
 //#include <sys/uio.h>
+
 int		get_next_line(const int fd, char **line);
 
 #endif
