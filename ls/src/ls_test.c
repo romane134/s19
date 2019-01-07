dba18614800b2cfafa53294800b0b14b238e2fd9
Romane Lucas de couville <rlucas-d@e-r5-p3.s19.be>
2019-01-07 18:54:14 +0100

    sg

---
+++ ls/src/ls_test.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   T_2.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smondesi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/13 14:33:09 by smondesi          #+#    #+#             */
+/*   Updated: 2019/01/07 18:53:13 by rlucas-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include "../includes/ft_ls.h"
+#include <time.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int		can_rec(char *flag, char *doc)
+{
+	struct stat *buf;
+
+	buf = (struct stat *)malloc(sizeof(struct stat));
+	stat(doc, buf);
+	if (buf->st_mode == 16877 && ft_strchr(flag, 'R') != NULL)
+		return (1);
+	return (0);
+}
+
+void	lecture(char *flag, char *doc)
+{
+	DIR				*dirp;
+	struct dirent	*dp;
+	char			*fludg;
+
+	if (!(dirp = opendir(doc)))
+	{
+		st_printf("Error: %s\n", strerror(errno));
+		//exit(0);
+	}
+	doc = ft_strjoin(doc, "/");
+	/*if ((readdir(dirp)) == NULL)
+	{
+			//st_printf("Error:  %s\n", strerror(errno));
+	}*/
+	while ((dp = readdir(dirp)) != NULL)
+	{
+		if (dp->d_name[0] != '.' || ft_strchr(flag, 'a') != NULL)
+		{
+			fludg = ft_strjoin(doc, dp->d_name);
+			if (ft_strchr(flag, 'l'))
+				analyse_file(fludg);
+			st_printf("%s\n", dp->d_name);
+			if (can_rec(flag, fludg) == 1)
+			{
+				if (ft_strcmp(dp->d_name, ".") != 0 &&
+						ft_strcmp(dp->d_name, "..") != 0)
+					lecture(flag, fludg);
+				st_printf("\n");
+			}
+		}
+	}
+	closedir(dirp);
+}
+
+void	which_flag(char **argv, int argc)
+{
+	int		i;
+	char	*flag;
+
+	i = 1;
+	flag = ft_strnew(0);
+	while (i < argc && argv[i][0] == '-')
+	{
+		flag = ft_strjoin(flag, argv[i]);
+		i++;
+	}
+	if (i == argc)
+		lecture(flag, ".");
+	else
+		while (i < argc)
+		{
+			lecture(flag, argv[i]);
+			i++;
+		}
+}
+
+int		main(int argc, char **argv)
+{
+	which_flag(argv, argc);
+	return (0);
+}
